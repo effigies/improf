8f3d3ec85343b47455fde1cf5866cfd3ef27d842
Christopher J. Markiewicz <markiewicz@stanford.edu>
2018-12-13 17:06:20 -0500

    ENH: Add profiler CLI

---
+++ import_profiler.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import sys
+import psutil
+import time
+from importlib import import_module
+
+P = psutil.Process()
+
+
+def fingerprint():
+    return {'modules': set(sys.modules),
+            'time': time.time(),
+            'memory': P.memory_info().vms / (1024 ** 2)}
+
+
+def diff_fingerprint(y, x):
+    return {'modules': y['modules'] - x['modules'],
+            'time': y['time'] - x['time'],
+            'memory': y['memory'] - x['memory']}
+
+
+def main():
+    module_list = [['sys']] + [arg.split(',') for arg in sys.argv[1:]]
+
+    if module_list == [['sys']]:
+        print("Usage: {} MODULE_GROUP [MODULE_GROUP [...]]")
+        sys.exit(0)
+
+    fingerprints = []
+
+    def load_module(*modules):
+        for module in modules:
+            import_module(module)
+        fingerprints.append((modules[0], fingerprint()))
+
+    for modules in module_list:
+        load_module(*modules)
+
+    diffs = [
+        (y[0], diff_fingerprint(y[1], x[1]))
+        for y, x in zip(fingerprints[1:], fingerprints[:-1])]
+    diffs.append(('Final', diff_fingerprint(fingerprints[-1][1], fingerprints[0][1])))
+
+    for name, diff in diffs:
+        print("{}: {} modules; {:.02f}ms, {:.01f}MiB".format(name, len(diff['modules']),
+                                                             diff['time'] * 1000,
+                                                             diff['memory']))
+
+    print('\nModules loaded with {}:'.format(diffs[-2][0]))
+    from pprint import pprint
+    pprint(diffs[-2][1]['modules'])
+
+
+if __name__ == '__main__':
+    main()
