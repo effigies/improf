d7ab35a3ee089572eaa4cbc03617ec17e079768d
Chris Markiewicz <effigies@gmail.com>
2023-01-03 09:57:21 -0500

    ENH: Profile improf loading, add --mem/--show options
    Allow users to skip memory collection to avoid loading psutil and its
    dependencies. There's no avoiding sys and time.
    
    Use --no-mem as the first flag to avoid memory and --mem to turn it on
    later in the list.
    
    Use --show after an import to show the modules loaded, and --no-show as
    the last argument to avoid showing the final set of modules loaded.

--- improf.py
+++ improf.py
@@ -1,55 +1,98 @@
 #!/usr/bin/env python
+__version__ = '0.2.0'
 import sys
-import psutil
 import time
-from importlib import import_module
-
-__version__ = '0.1.1'
-
-P = psutil.Process()
 
 
-def fingerprint():
-    return {'modules': set(sys.modules),
-            'time': time.time(),
-            'memory': P.memory_info().vms / (1024 ** 2)}
+def fingerprint(mem=True):
+    if mem:
+        mem = P().memory_info().vms / (1024**2)
+    return {'modules': set(sys.modules), 'time': time.time(), 'memory': mem}
+
+
+FINGERPRINTS = [
+    ('init', {'modules': set(), 'time': time.time(), 'memory': 0}),
+    ('sys', fingerprint(False)),
+]
+
+
+def P():
+    if P.ret is None:
+        import psutil
+
+        P.ret = psutil.Process()
+    return P.ret
+
+
+P.ret = None
 
 
 def diff_fingerprint(y, x):
-    return {'modules': y['modules'] - x['modules'],
-            'time': y['time'] - x['time'],
-            'memory': y['memory'] - x['memory']}
+    return {
+        'modules': y['modules'] - x['modules'],
+        'time': y['time'] - x['time'],
+        'memory': y['memory'] - x['memory'],
+    }
 
 
 def main():
-    module_list = [['sys']] + [arg.split(',') for arg in sys.argv[1:]]
+    module_list = [arg.split(',') for arg in sys.argv[1:]]
 
-    if module_list == [['sys']]:
+    if ['--help'] in module_list:
         import os
+
         cmd = os.path.basename(sys.argv[0])
-        print("Usage: {} MODULE_GROUP [MODULE_GROUP [...]]".format(cmd))
+        print('Usage: {} MODULE_GROUP [MODULE_GROUP [...]]'.format(cmd))
         sys.exit(0)
 
-    fingerprints = []
+    fingerprints = FINGERPRINTS.copy()
+    show = []
 
+    mem = module_list and module_list[0] != ['--no-mem']
+    if mem and 'psutil' not in module_list[0]:
+        module_list.insert(0, ['psutil'])
+    else:
+        module_list.pop(0)
     for modules in module_list:
+        show_last = True
+        if modules in (['--mem'], ['psutil']):
+            mem = True
+            modules = ['psutil']
+        elif modules == ['--show']:
+            show.append(fingerprints[-1][0])
+            continue
+        elif modules == ['--no-show']:
+            show_last = False
+            continue
         for module in modules:
             import_module(module)
-        fingerprints.append((modules[0], fingerprint()))
+        fingerprints.append((modules[0], fingerprint(mem)))
 
-    diffs = [
-        (y[0], diff_fingerprint(y[1], x[1]))
-        for y, x in zip(fingerprints[1:], fingerprints[:-1])]
-    diffs.append(('Final', diff_fingerprint(fingerprints[-1][1], fingerprints[0][1])))
+    if show_last:
+        show.append(fingerprints[-1][0])
 
-    for name, diff in diffs:
-        print("{}: {} modules; {:.02f}ms, {:.01f}MiB".format(name, len(diff['modules']),
-                                                             diff['time'] * 1000,
-                                                             diff['memory']))
+    diffs = {
+        y[0]: diff_fingerprint(y[1], x[1])
+        for y, x in zip(fingerprints[1:], fingerprints[:-1])
+    }
+    diffs['Final'] = diff_fingerprint(fingerprints[-1][1], fingerprints[0][1])
 
-    print('\nModules loaded with {}:'.format(diffs[-2][0]))
+    for name, diff in diffs.items():
+        print(
+            f'{name}: {len(diff["modules"])} modules; '
+            f'{diff["time"] * 1000:.02f}ms, {diff["memory"]:.01f}MiB'
+        )
+
     from pprint import pprint
-    pprint(diffs[-2][1]['modules'])
+
+    for name in show:
+        print(f'\nModules loaded with {name}:')
+        pprint(diffs[name]['modules'])
+
+
+from importlib import import_module
+
+FINGERPRINTS.append(('importlib', fingerprint(False)))
 
 
 if __name__ == '__main__':
